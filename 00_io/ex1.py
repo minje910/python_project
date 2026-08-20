d4bf14529639a4974721f67fc9553f29238a0ad7
C_programmer_mi <minje910@gmail.com>
2026-08-20 10:35:20 +0900

    first commit

---
+++ 00_io/ex1.py
@@ -0,0 +1,30 @@
+# # 입출력
+# a = input()
+# print(a, end="")
+# print(type(a))  # input() 함수는 항상 문자열을 반환합니다.
+
+# print(a, type(a), sep= ",")  # input() 함수는 항상 문자열을 반환합니다.
+
+# a = int(a)
+# print(a, end="")
+# print(type(a))  # input() 함수는 항상 문자열을 반환합니다.
+
+# print(a, type(a), sep= ",")  # input() 함수는 항상 문자열을 반환합니다.
+
+
+# a = int(input())
+
+# print(a, type(a))
+
+# b = float(input())
+# print(b, type(b))
+
+# #정수 2개 입력
+# #100
+# #200
+# a = int(input())
+# b = int(input())
+# print(a, b)
+# 100 200
+a = input().split()  # 공백을 기준으로 문자열을 나눠서 리스트로 반환합니다.`)
+print(a,type(a))
